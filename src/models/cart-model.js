29e92a9b376bdae316acf1d8fe0bcc841e5924a7
icazz <icha.zika.1@gmail.com>
2025-06-06 22:26:22 +0700

    update profile-model.js and cart-model.js

--- src/models/cart-model.js
+++ src/models/cart-model.js
@@ -1,1 +1,27 @@
-// TODO: implement cart model
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    quantity: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    profile: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Profile",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Cart = mongoose.model("Cart", schema);
--- src/models/profile-model.js
+++ src/models/profile-model.js
@@ -1,1 +1,40 @@
-// TODO: implement profile model
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    full_name: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Profile = mongoose.model("Profile", schema);

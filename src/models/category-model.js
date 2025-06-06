e1b488e10e2d83efb26f4a6cbe32f46455a4e799
unknown <hansenc0710@gmail.com>
2025-06-06 16:33:31 +0700

    update category-model and payment-model

--- src/models/category-model.js
+++ src/models/category-model.js
@@ -1,1 +1,24 @@
 // TODO: implement category model
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Category = mongoose.model("Category", schema);
--- src/models/payment.js
+++ src/models/payment.js
@@ -1,1 +1,26 @@
 // TODO: implement payment model
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    payment_method: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    payment_date: {
+        type: Date,
+        required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Payment = mongoose.model("Payment", schema);
